f9e3d788f0352afd0df99fdd1fae0431e7594d71
Rana Zeeshan Ali <ra24adi@herts.ac.uk>
2026-02-28 08:09:08 +0500

    Updated IPR

--- RL_Unity_Pro/Assets/EasyRoads3D/lib/ERTutorial.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-using System.Collections.Generic;
-
-public class ERTutorial : MonoBehaviour {
-
-	public string[] linkText = new string[0];
-	public string[] links = new string[0];
-}
